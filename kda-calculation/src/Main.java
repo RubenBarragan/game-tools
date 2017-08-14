5f57f759aa787858a15d96240583fab2ed070bff
Rubén Barragán <RubenBarragan@users.noreply.github.com>
2017-08-13 21:29:40 -0500

    Changes and Rate-Limit Control System
    Minor changes to the code and an addition of a rate-limit control system that controls the API exceeding.

--- kda-calculation/src/Main.java
+++ kda-calculation/src/Main.java
@@ -1,30 +1,47 @@
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.time.Duration;
 import java.util.Scanner;
-
 import org.json.*;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BlockingStrategy;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
 
 public class Main {
 	
 	static String KEY = "RGAPI-4e7c07e3-8e0f-400d-b187-51d8b8e38647";
 	static String MAIN_URL = "https://la1.api.riotgames.com";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// Define the limit 20 times per 1 second
+		// These lines are used to control rate limit.
+		Bandwidth limit = Bandwidth.simple(100, Duration.ofMinutes(3));
+		Bucket bucket = Bucket4j.builder().addLimit(0, limit).build();
+		
+		// Consume one API request (rate limit control system)
+		bucket.consume(1, BlockingStrategy.PARKING);
 		// For each participant in the current match.
-		for(Object p : getCurrentListParticipantBySummonerId(getSummonerByName("Crow22").getLong("id"))) {
+		for(Object p : getCurrentListParticipantBySummonerId(getSummonerByName("MaikelRyuu").getLong("id"))) {
+			
 			
 			// Participant object.
 			JSONObject participant = (JSONObject) p;
 			
+			// Consume one API request (rate limit control system)
+			bucket.consume(1, BlockingStrategy.PARKING);
 			// Summoner object.
 			JSONObject summoner = getSummonerBySummonerId(participant.getLong("summonerId"));
 			
 			System.out.println(summoner.get("name"));
 			
+			// Consume one API request (rate limit control system)
+			bucket.consume(1, BlockingStrategy.PARKING);
 			// Object about the 20 last matches for summoner "summoner".
 			JSONObject matchesObject = getLast20MatchesByAccountId(summoner.getLong("accountId"));
+			
+			int count = 0;
 			
 			// For each match
 			for(Object m : matchesObject.getJSONArray("matches")) {
@@ -32,15 +49,13 @@ public class Main {
 				// Match obtained from matches.
 				JSONObject matchReference = (JSONObject) m;
 				
+				// Consume one API request (rate limit control system)
+				bucket.consume(1, BlockingStrategy.PARKING);
 				// Match object (detailed information)
 				JSONObject match = getMatchByMatchID(matchReference.getLong("gameId"));
 				
-				
-				
-				System.out.println(match);
+				System.out.println(++count);
 			}
-			
-			System.out.println();
 		}
 	}
 	
